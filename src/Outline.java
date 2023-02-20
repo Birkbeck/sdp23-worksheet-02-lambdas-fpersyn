c27fcfc23f9ef71e8bb0e1e0cdbbf4224d51aff2
Fred Persyn <frederic.persyn@gmail.com>
2023-02-20 21:20:55 +0000

    Added Q1A

--- src/Outline.java
+++ src/Outline.java
@@ -1,9 +1,47 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1,7,3,4,8,2};
-    System.out.println(Arrays.asList(intArray));
-    // Arrays.sort(intArray,.......)
-  } 
+
+  public static String[] words() {
+      return new String[] {
+          "face",
+          "wolf",
+          "compass",
+          "elephant",
+          "angular",
+          "spaceman",
+          "santa",
+          "camera"
+      };
+  }
+
+  public static void question_1a() {
+    // sort words by length (ascending)
+    String[] words = words();
+    Arrays.sort(words, (String a, String b) -> Integer.compare(a.length(), b.length()));
+    System.out.println("Q1A = " + Arrays.asList(words));
+  }
+
+  public static void question_1b() {
+    // sort words by length (descending)
+    String[] words = words();
+    // TODO
+  }
+
+  public static void question_1c() {
+    // sort words by first character
+    String[] words = words();
+    // TODO
+  }
+
+  public static void question_1d() {
+    // sort words by those that start with "e", then remaining letters.
+    String[] words = words();
+    // TODO
+  }
+
+  public static void main(String... args) {
+    question_1a();
+  }
 }
